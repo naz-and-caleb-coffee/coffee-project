da11168db7eea269f0e993e8ea327f111b728c35
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-09-19 22:57:35 -0500

    Added anchor to cards

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 function renderCoffee(coffee) {
     var html = ` <div class="card">
-        <div class="card-body">
+        <a href="#"><div class="card-body">
             <h5 class="card-subtitle mb-2 text-muted">${coffee.name}</h5>
             <p class="card-text">${coffee.roast.toUpperCase()}</p> 
-        </div>
+        </div></a>
     </div>`
 
     return html;
